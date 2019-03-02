b0e5e9fba88fea3a6998b859f579fbdad5a27e9f
Oleksandra88 <ok.007@wp.pl>
2019-03-02 15:44:07 +0100

    Controller

--- src/main/java/pl/teb/edukacja/wiosna/GreetingController.java
+++ src/main/java/pl/teb/edukacja/wiosna/GreetingController.java
@@ -1,6 +1,7 @@
 package pl.teb.edukacja.wiosna;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class GreetingController {
     private static final String template = "Hello , %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(
             @RequestParam(value = "name", defaultValue = "Hello")
                     String name) {
