fa37285039e51f5641982b0e0aaef53ec108d825
Oleksandra88 <ok.007@wp.pl>
2019-03-02 12:48:01 +0100

    spring

---
+++ src/main/java/pl/teb/edukacja/wiosna/WiosnaApplication.java
@@ -0,0 +1,13 @@
+package pl.teb.edukacja.wiosna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiosnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiosnaApplication.class, args);
+	}
+
+}
