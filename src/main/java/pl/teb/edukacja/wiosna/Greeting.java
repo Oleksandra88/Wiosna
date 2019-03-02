361c5e46231189d479f0a9c5199e66a699352e56
Oleksandra88 <ok.007@wp.pl>
2019-03-02 14:56:03 +0100

    Controller

---
+++ src/main/java/pl/teb/edukacja/wiosna/Greeting.java
@@ -0,0 +1,22 @@
+package pl.teb.edukacja.wiosna;
+
+public class Greeting {
+    public long getId() {
+        return id;
+    }
+
+    private final long id;
+
+    public String getContent() {
+        return content;
+    }
+
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+
+}
